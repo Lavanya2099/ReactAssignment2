631455e4d038fb9783588206eddd72fe0da443a6
Lavanya Chandrappa <106539@tfcorp.com>
2021-12-29 13:57:00 +0530

    commit changes

--- 4.FetchData/fetch.js
+++ 4.FetchData/fetch.js
@@ -1,11 +1,18 @@
-let url= 'https://jsonplaceholder.typicode.com/posts'
+let url='https://jsonplaceholder.typicode.com/posts'
 
-async function fetchPosts(){
-    let response = await fetch(url)
-    let data = await response.json();
-
+async function fetchUrl(){
+    let response=await fetch(url)
+    let data=await response.json()
     console.log(data);
+    appendData(data)
 }
 
-
-fetchPosts()
+function appendData(data) {
+    var mainContainer = document.getElementById("container");
+    for (var i = 0; i < data.length; i++) {
+      var div = document.createElement("div");
+      div.innerHTML = `Title ${i+1}: ` + data[i].title;
+      mainContainer.appendChild(div);
+    }
+  }
+fetchUrl();
